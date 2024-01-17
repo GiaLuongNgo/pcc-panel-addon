31acd044cbcc7e50b8f5e721cbca4ed04a61d621
luong.ngo <luong.ngo@smartbox.com>
2024-01-17 16:49:23 +0700

    fix version

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { useAddonState, useChannel, useParameter } from "@storybook/manager-api";
+import { useStorybookApi } from "@storybook/api";
 import { AddonPanel } from "@storybook/components";
-import { ADDON_ID, EVENTS, PARAM_KEY } from "./constants";
-
 
 interface PanelProps {
   active: boolean;
 }
 
 export const Panel: React.FC<PanelProps> = (props) => {
-  // https://storybook.js.org/docs/react/addons/addons-api#useaddonstate
-  const args = useParameter(PARAM_KEY, {});
-  const text = JSON.stringify(args, null, 2);
+  const api = useStorybookApi();
+
+  const story = api.getCurrentStoryData();
+
+  if (!story) {
+    return null;
+  }
 
   return (
     <AddonPanel {...props}>
-      <p>{text}</p>
+      <p>{JSON.stringify(story.args)}</p>
     </AddonPanel>
   );
 };
--- src/manager.ts
+++ src/manager.ts
@@ -1,8 +1,7 @@
-import { addons, types } from "@storybook/manager-api";
+import { addons, types } from "@storybook/addons";
 import { ADDON_ID, TOOL_ID, PANEL_ID, TAB_ID } from "./constants";
 import { Tool } from "./Tool";
 import { Panel } from "./Panel";
-import { Tab } from "./Tab";
 
 /**
  * Note: if you want to use JSX in this file, rename it to `manager.tsx`
@@ -25,16 +24,5 @@ addons.register(ADDON_ID, () => {
     title: "panel",
     match: ({ viewMode }) => viewMode === "story",
     render: Panel,
-  });
-
-  // Register the tab
-  addons.add(TAB_ID, {
-    type: types.TAB,
-    title: "My addon",
-    //👇 Checks the current route for the story
-    route: ({ storyId }) => `/myaddon/${storyId}`,
-    //👇 Shows the Tab UI element in myaddon view mode
-    match: ({ viewMode }) => viewMode === "myaddon",
-    render: Tab,
   });
 });
