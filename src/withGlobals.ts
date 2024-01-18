4a2b2582859e69dd257788e88430d5b94f34c2be
luong.ngo <luong.ngo@smartbox.com>
2024-01-19 00:04:09 +0700

    delete preview

--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -1,25 +1,24 @@
 import type { DecoratorFunction } from "@storybook/addons";
-import { useEffect, useGlobals } from "@storybook/addons";
 
 export const withGlobals: DecoratorFunction = (StoryFn, context) => {
-  const [{ myAddon }] = useGlobals();
-  // Is the addon being used in the docs panel
-  const isInDocs = context.viewMode === "docs";
-  const { theme } = context.globals;
+  // const [{ myAddon }] = useGlobals();
+  // // Is the addon being used in the docs panel
+  // const isInDocs = context.viewMode === "docs";
+  // const { theme } = context.globals;
 
-  useEffect(() => {
-    // Execute your side effect here
-    // For example, to manipulate the contents of the preview
-    const selectorId = isInDocs
-      ? `#anchor--${context.id} .docs-story`
-      : `#root`;
+  // useEffect(() => {
+  //   // Execute your side effect here
+  //   // For example, to manipulate the contents of the preview
+  //   const selectorId = isInDocs
+  //     ? `#anchor--${context.id} .docs-story`
+  //     : `#root`;
 
-    displayToolState(selectorId, {
-      myAddon,
-      isInDocs,
-      theme,
-    });
-  }, [myAddon, theme]);
+  //   displayToolState(selectorId, {
+  //     myAddon,
+  //     isInDocs,
+  //     theme,
+  //   });
+  // }, [myAddon, theme]);
 
   return StoryFn();
 };
